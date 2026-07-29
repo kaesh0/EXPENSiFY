1a194513f9b0f47efb0865e9efdcbff989177d37
kaesh0 <aaayushyadav632@gmail.com>
2026-07-29 20:44:32 +0530

    Prepare backend for production deployment

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json());
 app.use(cookie_parser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
     res.send("Server is working");
 
 });
-app.listen(3000,()=>{
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>{
     console.log("SERVER RUNNING AT PORT:3000")
 })
